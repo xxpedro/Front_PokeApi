a80fc21b10650175a7e0afe3c257230644016483
xxpedro <pedrom.g712@gmail.com>
2023-12-13 10:03:01 -0400

    Adding Not Found' Page in Routes

--- front_pokeApi/src/app/app-routing.module.ts
+++ front_pokeApi/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PagesRoutingModule } from './pages/pages.routing';
+import { PageNotFoundComponent } from './404/page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
+  {path:'**', component:PageNotFoundComponent},
 ];
 
 @NgModule({
--- front_pokeApi/src/app/app.module.ts
+++ front_pokeApi/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 import { ComponentsModule } from './components/components.module';
+import { PageNotFoundComponent } from './404/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageNotFoundComponent,
 
   ],
   imports: [
